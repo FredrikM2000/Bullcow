b35d66b597de53bcf97bd35aec9697bd8654d0d3
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2022-05-31 19:31:23 +0200

    Final lecture

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -9,21 +9,18 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
     // *********Only write code under this line *************
 
+
     const FString WordListPath = FPaths::ProjectContentDir() / TEXT("WordList.txt");
-    FFileHelper::LoadFileToStringArray(Words, *WordListPath);
+    FFileHelper::LoadFileToStringArrayWithPredicate(Isograms, *WordListPath, [](const FString& Word)
+        {
+            return Word.Len() >= 4 && Word.Len() <= 8 && IsIsogram(Word);
+        });
 
 
     SetupGame();
-    PrintLine(TEXT("The number of possible words is %i"), Words.Num());
-    PrintLine(TEXT("The number of valid words is %i"), GetValidWords(Words).Num());
-    PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord);//Debug line
-
-    TArray<FString> ValidWords = GetValidWords(Words);
-
- 
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& PlayerInput) // When the player hits enter
 {
     if (bGameOver)
     {
@@ -32,7 +29,7 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     }
     else
     {
-        ProcessGuess(Input);
+        ProcessGuess(PlayerInput);
     }
 }
 
@@ -40,11 +37,13 @@ void UBullCowCartridge::SetupGame()
 {
     PrintLine(TEXT("Welcome"));
 
-    HiddenWord = TEXT("cakes");
+    HiddenWord = Isograms[FMath::RandRange(0, Isograms.Num() - 1)];
     Lives = HiddenWord.Len();
     bGameOver = false;
 
     PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord);//Debug line
+
     PrintLine(TEXT("You have %i lives"), Lives);
     PrintLine(TEXT("Type your guess and\npress enter to continue..."));
 }
@@ -55,7 +54,7 @@ void UBullCowCartridge::EndGame()
     PrintLine(TEXT("\nPress enter to play again"));
 }
 
-void UBullCowCartridge::ProcessGuess(FString Guess)
+void UBullCowCartridge::ProcessGuess(const FString& Guess)
 {
     if (Guess == HiddenWord)
     {
@@ -90,11 +89,16 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         return;
     }
 
+    // Show the player Bulls an Cows
+    FBullCowCount Score = GetBullCows(Guess);
+
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
+
     PrintLine(TEXT("Guess again, you have %i lives left"), Lives);
 }
 
 
-bool UBullCowCartridge::IsIsogram(FString word) const
+bool UBullCowCartridge::IsIsogram(const FString& word)
 {
     for (int32 Index = 0; Index < word.Len() - 1; Index++)
     {
@@ -107,7 +111,7 @@ bool UBullCowCartridge::IsIsogram(FString word) const
     return true;
 }
 
-TArray<FString> UBullCowCartridge::GetValidWords(TArray<FString> WordList) const
+TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList) const
 {
     TArray<FString> ValidWords;
 
@@ -117,14 +121,27 @@ TArray<FString> UBullCowCartridge::GetValidWords(TArray<FString> WordList) const
             ValidWords.Emplace(Word);
         }
     }
-
-
-    //for (int32 i = 0; i < WordList.Num(); i++)
-    //{
-    //    if (WordList[i].Len() >= 4 && WordList[i].Len() <= 8 && IsIsogram(WordList[i]))
-    //    {
-    //        ValidWords.Emplace(WordList[i]);
-    //    }
-    //}
     return ValidWords;
+}
+
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
+{
+    FBullCowCount Count;
+
+    for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
+    {
+        if (Guess[GuessIndex] == HiddenWord[GuessIndex])
+        {
+            Count.Bulls++;
+            continue;
+        }
+
+        for (int32 HiddenIndex = 0; HiddenIndex < HiddenWord.Len(); HiddenIndex++)
+            if (Guess[GuessIndex] == HiddenWord[HiddenIndex])
+            {
+                Count.Cows++;
+                break;
+            }
+    }
+    return Count;
 }
